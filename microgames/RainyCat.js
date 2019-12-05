405c4da755f8a3075504f58b9f9edcb0219a4620
Lucas Mun <cmun09@vt.edu>
2019-12-04 21:21:00 -0500

    Finished RainyCat minigame

--- microgames/RainyCat.js
+++ microgames/RainyCat.js
@@ -8,10 +8,15 @@ class RainyCat extends Microgame{
 	curCatPos = 300;
 	curCatDir = (Math.round(Math.random())) ? -1 : 1;
     distance = 10;
-    catFrame = 1;
+    catFrame = -1;
     gaitChange = 0;
     catWet = 0;
     catWetTimer = 0;
+    tutorialTimer = 0;
+    arrow_timer = 0;
+    arrow_blink = 1;
+    blink = 150;
+
     frames = [
     { // walk 1
         "start": [80,58],
@@ -56,25 +61,24 @@ class RainyCat extends Microgame{
     ]
 
 
-    update(delta) {
+    playGame(delta){
         this.flipTimer += delta;
         this.catWetTimer += delta;
+        
+        if(this.catFrame !== 0){
+            if (this.game.keys['right']) {
+                if(this.umbPos < 420){
+                    this.umbPos+=4;
+                }
 
-    	if(this.catFrame !== 0){
-	        if (this.game.keys['right']) {
-	        	if(this.umbPos < 420){
-		            this.umbPos+=4;
-	        	}
-
-	        } 
-	        else if (this.game.keys['left']) {
-	    		if(this.umbPos > 10){
-	            	this.umbPos-=4;
-	        	}
-
-	        } 	
-    	}
-    	// keypresses
+            } 
+            else if (this.game.keys['left']) {
+                if(this.umbPos > 10){
+                    this.umbPos-=4;
+                }
+            }   
+        }
+        // keypresses
 
 
         if(this.flipTimer > 50){
@@ -83,48 +87,66 @@ class RainyCat extends Microgame{
         }
 
         if(this.catWetTimer > 100){
-        	this.catWetTimer = 0;
-        	this.catWet = !this.catWet;
+            this.catWetTimer = 0;
+            this.catWet = !this.catWet;
         }
 
 
         // check if it hit left boundary
         if((this.curCatPos + 30 < this.umbPos) || (this.curCatPos + 46 > this.umbPos + 185 + 20)){
-        	this.distance = -1;
-        	this.catFrame = 0;
-	    	//this.curCatPos += this.curCatDir * 6;
+            this.distance = -1;
+            this.catFrame = 0;
+            //this.curCatPos += this.curCatDir * 6;
         }// 185 is the width of the umbrella
 
         else{
-	        if(this.distance === 0){
-	        	this.distance = randInt(15, 30);
-	        	this.curCatDir *= -1;
-	        }
-	        else if(this.curCatPos < 30 && this.curCatPos > 0){
-	        	this.distance = randInt(15, 30);
-	    		this.curCatDir = 1;
-	    	}
-	    	else if(this.curCatPos > 540){
-	        	this.distance = randInt(15, 30);
-	    		this.curCatDir = -1;
-	    	}
+            if(this.distance === 0){
+                this.distance = randInt(15, 30);
+                this.curCatDir *= -1;
+            }
+            else if(this.curCatPos < 30 && this.curCatPos > 0){
+                this.distance = randInt(15, 30);
+                this.curCatDir = 1;
+            }
+            else if(this.curCatPos > 540){
+                this.distance = randInt(15, 30);
+                this.curCatDir = -1;
+            }
 
-	        if(this.gaitChange > 30){
-		        this.catFrame *= -1;
-		        this.gaitChange = 0;
-	        }
-	        
-	    	this.curCatPos += this.curCatDir * 3;
-	        this.distance -= 3;
-	        this.gaitChange += 3;
+            if(this.gaitChange > 30){
+                this.catFrame *= -1;
+                this.gaitChange = 0;
+            }
+            
+            this.curCatPos += this.curCatDir * 3;
+            this.distance -= 3;
+            this.gaitChange += 3;
         }
+    }
 
-
+    tutorial(delta){
+        this.arrow_timer += delta;
+        if (this.arrow_timer > this.blink) {
+            this.arrow_blink *= -1;
+            this.arrow_timer = 0;
+        }
+    }
+    update(delta) {
+        this.tutorialTimer += delta;
+        if(this.tutorialTimer > 900){
+            this.playGame(delta);
+        }
+        else{
+            this.tutorial(delta);
+        }
     }
 
 
     draw() {    
+        warioText("MOVE UMBRELLA!");
+
         p.background(0, 0, 0);
+
         p.pushMatrix();
         p.scale(1.3);
         imageFrame(SPRITES.rainyCat.sheet, this.frames[6]); // background
@@ -192,6 +214,24 @@ class RainyCat extends Microgame{
         p.scale(1.5);
         imageFrame(SPRITES.rainyCat.sheet, this.frames[5]); // umbrella
         p.popMatrix();
+
+
+        if(this.tutorialTimer < 900){
+            p.pushMatrix();
+            p.translate(70, 80);
+            p.scale(1);
+            warioText("KEEP THE CAT DRY!");
+            p.popMatrix();
+
+            if (this.arrow_blink > 0) {
+                p.pushMatrix();
+                p.translate(265, 200);
+                p.scale(3);
+                p.image(SPRITES.text.get(673, 215, 28, 20), 0, 0);
+                p.popMatrix();
+            }
+        }
+
 
 
     }
