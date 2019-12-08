e9099ec6d538125a032f98f1540200c5ac89431d
Brandon Walker <brandon@branw.xyz>
2019-12-07 21:12:05 -0500

    Enable all games

--- microgames/all.js
+++ microgames/all.js
@@ -1,4 +1,4 @@
-const microgames = [WrongWayHighway]; //LinkChase];//BananaMunch, DuckHunt, Shake, RainyCat];
+const microgames = [WrongWayHighway, BananaMunch, DuckHunt, Shake, RainyCat]; // LinkChase
 
 function randomMicrogame() {
     return microgames[Math.floor(Math.random()*microgames.length)];
